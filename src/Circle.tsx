49b676db126d74c6cf96b87d1b8409f633f6b4d4
mamonde456 <mamonde456@gmail.com>
2022-04-19 15:36:29 +0900

    first commit

---
+++ src/Circle.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor = "default text" }: CircleProps) {
+  const [value, setValue] = useState<number | string>(1);
+  setValue(5);
+  setValue("test");
+  return (
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ?? bgColor}
+    ></Container>
+  );
+}
+
+export default Circle;
